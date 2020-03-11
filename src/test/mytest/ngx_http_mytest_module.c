d6441173f8a0895bf24044c46917bdaf5dab7c11
gzhang91 <tanghebinglin@126.com>
2020-03-11 03:49:05 +0000

    1. delete objs

--- src/test/mytest/ngx_http_mytest_module.c
+++ src/test/mytest/ngx_http_mytest_module.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include "ngx_http.h"
 
+typedef struct {
+	ngx_str_t my_str;
+	ngx_int_t my_num;
+	ngx_flag_t my_flag;
+	size_t my_size;
+	ngx_array_t *my_str_array;
+	ngx_array_t *my_keyval;
+	off_t my_off;
+	ngx_msec_t my_msec;
+	time_t my_sec;
+	ngx_bufs_t my_bufs;
+	ngx_uint_t my_enum_seq;
+	ngx_uint_t my_bitmask;
+	ngx_uint_t my_access;
+	ngx_path_t *my_path;
+} ngx_http_mytest_conf_t;
+
 static char *ngx_http_mytest(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t *r);
 
